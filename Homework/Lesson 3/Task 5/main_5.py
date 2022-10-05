4dfacb0bf8de98297e31aa3fdbcacee10dd2ec04
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-10-05 21:40:38 +0300

    добавил main_5.py

---
+++ Homework/Lesson 3/Task 5/main_5.py
@@ -0,0 +1,10 @@
+number = int(input("Введите число: "))
+list_fibonacci  = [0, 1]
+
+for i in range(2, number + 1):
+    list_fibonacci.append(list_fibonacci[i-1] + list_fibonacci[i-2])
+
+for i in range(number):
+    list_fibonacci.insert(0, list_fibonacci[1] - list_fibonacci[0])
+
+print(f'Для k = {number} список чисел Фибоначчи: {list_fibonacci}')
