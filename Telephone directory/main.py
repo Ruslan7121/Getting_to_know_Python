eebbca665f481513c28aa0dcf7a5802beacec213
Каверин Руслан Андреевич <salat_71@mail.ru>
2022-10-21 18:22:38 +0300

    создал main.py

---
+++ Telephone directory/main.py
@@ -0,0 +1,6 @@
+# Телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах
+
+from controller import work_with_phonebook
+
+if __name__ == "__main__":
+    work_with_phonebook()
